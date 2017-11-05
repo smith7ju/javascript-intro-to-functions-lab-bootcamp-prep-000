9aa94711a072a666479947db3725666d7362e695
Josh Smith <smith7ju@gmail.com>
2017-11-05 20:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ function logWhisper(string)
   console.log(string.toLowerCase());
 }
 
+function sayHiToGrandma(string)
+{
+  if(string.toUpperCase() === string)
+    return "I can't hear you!"
+  else
+    return "YES INDEED!"
+}
