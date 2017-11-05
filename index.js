8e3961a177f47ff7f32cd52da28e7a430c3321bb
Josh Smith <smith7ju@gmail.com>
2017-11-05 20:50:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logWhisper(string)
 function sayHiToGrandma(string)
 {
   if(string == "I love you, Grandma.")
-    return 
+    return "I love you, too."
   else {
     if(string.toLowerCase() === string)
       return "I can't hear you!"
